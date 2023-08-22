0d065bdbfbd1f85ba06471e8dbc44b48755a46fb
iatopilskii <iatopilskii@gmail.com>
2023-08-22 16:59:43 +0300

    make solutions better

--- problems/242_valid_anagram/index.ts
+++ problems/242_valid_anagram/index.ts
@@ -1,12 +1,22 @@
 function isAnagram(s: string, t: string): boolean {
-  const sSorted = s.split('').sort().join('')
-  const tSorted = t.split('').sort().join('')
-
-  if (tSorted === sSorted) {
-    return true
+  if (s.length !== t.length) {
+    return false
   }
 
-  return false
+  let freq = new Array(26).fill(0)
+
+  for (let i = 0; i < s.length; i++) {
+    freq[s[i].charCodeAt(0) - 'a'.charCodeAt(0)]++
+    freq[t[i].charCodeAt(0) - 'a'.charCodeAt(0)]--
+  }
+
+  for (let i = 0; i < freq.length; i++) {
+    if (freq[i] !== 0) {
+      return false
+    }
+  }
+
+  return true
 }
 
 export default isAnagram
